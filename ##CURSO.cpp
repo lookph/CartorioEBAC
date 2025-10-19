4d280f6f79ab7a5ebf9dad6d074ac416b2f12daf
lookph <mr2007antonio@gmail.com>
2025-10-19 11:22:56 -0300

    Add files via upload

--- ##CURSO.cpp
+++ ##CURSO.cpp
@@ -127,8 +127,8 @@ int main()
 	  printf("\t1- Registrar nomes\n");
       printf("\t2- Consultar nomes\n");
       printf("\t3- Deletar nomes\n\n");// o final do menu
+      printf("\t4- Sair do sistema\n\n");
 	  printf("escolha uma opção:");
-	
 	  scanf("%d", &opcao); //armazenamento de informações do usuário
 	
 	  system("cls");//Responsável por limpar a tela
@@ -146,6 +146,11 @@ int main()
 		 case 3:
 		  Deletar();
 		  break;
+		
+		 case 4:
+		  printf("Obrigado por ultilizar o sistema!\n");
+		  return 0;
+		  break;	
 			
 		 default:
 		    printf("Essa opção não está disponível\n");
